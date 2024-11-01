e13e3cce4768e514966323e5dad86805291122fe
SANSKAR JAISWAL <94032820+Sanskarjaas@users.noreply.github.com>
2024-11-01 21:40:25 +0530

    provider Updated

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+terraform{
+  required_providers{
+    azurerm={
+      source="hashicorp/azurerm"
+      }
+  }
+}
 resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "South India"
