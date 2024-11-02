d8cbd6b7a139ce5585e1cfc395048cdec69a967d
SANSKAR JAISWAL <94032820+Sanskarjaas@users.noreply.github.com>
2024-11-03 01:40:58 +0530

    updated the subscription and added the tenant and client secret.

--- provider.tf
+++ provider.tf
@@ -8,4 +8,8 @@ terraform {
 }
 provider "azurerm" {
   features {}
+  subscription_id = "3e2875de-c6b7-4fb2-a981-e0c5cacb7925"
+  client_id       = "<appId from service principal>"
+  client_secret   = "<password from service principal>"
+  tenant_id       = "40c09225-3c1a-4894-8200-f1b020147494"
 }
